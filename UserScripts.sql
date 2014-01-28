d3e21d2e0517288fb9b797d8be021db772f5784b
Avrumi Friedman <datmonkey2002@yahoo.com>
2014-01-27 21:10:07 -0500

    Added user script

---
+++ UserScripts.sql
@@ -0,0 +1,32 @@
+USE [CustomAuth]
+GO
+
+/****** Object:  Table [dbo].[Users]    Script Date: 1/27/2014 9:09:33 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Users](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [varchar](100) NOT NULL,
+	[LastName] [varchar](100) NOT NULL,
+	[Email] [varchar](150) NOT NULL,
+	[Username] [varchar](50) NOT NULL,
+	[Password] [varchar](150) NOT NULL,
+	[Salt] [varchar](150) NOT NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
